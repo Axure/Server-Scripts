30e69df755c44d777f29f52d46ef70b4d1c0d653
Axure <freetiger18@gmail.com>
2015-08-20 12:43:22 -0400

    Added locale fix.

---
+++ locale.sh
@@ -0,0 +1,11 @@
+locale
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "LANG=en_US.UTF-8" >> /etc/environment
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_TYPE=en_US.UTF-8
+sudo locale-gen "en_US.UTF-8"
+sudo locale-gen
+sudo dpkg-reconfigure locales
+locale
